98d3e9f4a2b25c81e2874bc3913a6f7bdf8a36fc
Debra Kuang <dekuang2@gmail.com>
2025-11-18 12:09:11 -0800

    lab 8 commits pt 1 (hopefully the index updates this time???)

--- garden/creature.js
+++ garden/creature.js
@@ -1,8 +1,30 @@
 //a variable for storing names
 
 //when the button is clicked (event)
-$("button #crAdd").click(function(){
+$("#crAdd").click(function(){
     
-    alert("button is clicked");
-})
+    //$("#creature-list").html("button is clicked");
+    let crName = $("#crNameInput").val();
+    let crColor = $("#crColorInput").val();
+    let crEyesNum = $("#crEyeInput").val();
+    let crEyesHTML = "";
+
+    for (let i=0; i<crEyesNum; i++){
+        crEyesHTML = crEyesHTML + '<div class = eye>.</div>';
+    }
+
+    if ((crName != "") || (crName.length < 11)){
+        /* old code
+        $("#creature-list").append(crName + ", " + crColor + ", " + crEyes);
+        $("#crNameInput").val('done!');*/ 
+        //$("#crNameInput").html("done!") doesnt work for whatever reason)
+        $("#creature-list").append(
+            `<div class="creature">
+                <div class="creature-body" style = "background: ${crColor}">${crEyesHTML}</div>
+                <div class="creature-info">${crName}</div>
+            </div>`
+        )
+    }
+    
+});
 //the magic move(?)
